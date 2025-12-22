467d2513bf6e046b60620e7c4f16f0bf978158af
Jeff Hiner <jeff@preludesecurity.com>
2025-12-21 20:05:58 -0700

    Update batchfile to call Make

--- build_wasm.bat
+++ build_wasm.bat
@@ -1,45 +1,15 @@
 @echo off
 setlocal
 
-set "EMSDK=C:\Users\prelu\git\emsdk"
-set "PATH=%EMSDK%\upstream\emscripten;%PATH%"
-
-echo.
-echo === Building SEG-MAT WASM Buffer API ===
-echo.
+REM Build SEG-MAT WASM using Make
+REM Usage:
+REM   build_wasm.bat           - Build with -O2
+REM   build_wasm.bat OPT=-O0   - Build without optimizations (for debugging)
+REM   build_wasm.bat -j4       - Parallel build
+REM   build_wasm.bat clean     - Remove build artifacts
 
 cd /d "%~dp0"
 
-em++ -O2 -std=c++14 -s WASM=1 ^
-    -s EXPORTED_FUNCTIONS="['_wasm_malloc','_wasm_free','_segmat_segment']" ^
-    -s EXPORTED_RUNTIME_METHODS="['ccall','cwrap']" ^
-    -s ALLOW_MEMORY_GROWTH=0 ^
-    -s INITIAL_MEMORY=134217728 ^
-    -I src ^
-    -I src/emd ^
-    -I src/graphcut ^
-    -I src/ombb ^
-    -I include ^
-    src/Decomposer.cpp ^
-    src/MAT-geometry.cpp ^
-    src/MAT-main.cpp ^
-    src/emd/emd.cpp ^
-    src/graphcut/GCoptimization.cpp ^
-    src/graphcut/LinkedBlockList.cpp ^
-    src/graphcut/graph.cpp ^
-    src/graphcut/maxflow.cpp ^
-    src/ombb/gdiam.cpp ^
-    segmat_wasm_api.cpp ^
-    -o segmat_buffer.js
-
-if %ERRORLEVEL% EQU 0 (
-    echo.
-    echo === WASM compilation successful! ===
-    echo Output: segmat_buffer.js and segmat_buffer.wasm
-    dir segmat_buffer.*
-) else (
-    echo.
-    echo === WASM compilation FAILED ===
-)
+make %*
 
 endlocal
